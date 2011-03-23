6e6a36a2a34fe5d871903eb7e0c9abe0e2b1eb19
Brad Buran <bburan@users.noreply.github.com>
2011-03-23 15:45:34 -0400

    Bugfix to writing of data to hardware via DSP process

---
+++ convert.py
@@ -0,0 +1,91 @@
+import re
+
+class SamplingRateError(ValueError):
+    '''
+    Indicates that the conversion of frequency to sampling rate could not be
+    performed.
+    '''
+
+    def __init__(self, fs, requested_fs):
+        self.fs = fs
+        self.requested_fs = requested_fs
+
+    def __str__(self):
+        mesg = 'The requested sampling rate, %f Hz, is greater than ' + \
+               'the DSP clock frequency of %f Hz.'
+        return mesg % (self.requested_fs, self.fs)
+
+def convert(src_unit, dest_unit, value, dsp_fs):
+    '''
+    Converts value to desired unit give the sampling frequency of the DSP.
+
+    Parameters specified in paradigms are typically expressed as
+    frequency and time while many DSP parameters are expressed in number of
+    samples (referenced to the DSP sampling frequency).  This function provides
+    a convenience method for converting between conventional values and the
+    'digital' values used by the DSP.
+
+    Note that for converting units of time/frequency to n/nPer, we have to
+    coerce the value to a multiple of the DSP period (e.g. the number of 'ticks'
+    of the DSP clock).
+
+    Appropriate strings for the unit types:
+
+        fs
+            sampling frequency
+        nPer
+            number of samples per period
+        n
+            number of samples
+        s
+            seconds
+        ms
+            milliseconds
+        nPow2
+            number of samples, coerced to the next greater power of 2 (used for
+            ensuring efficient FFT computation)
+
+    >>> convert('s', 'n', 0.5, 10000)
+    5000
+    >>> convert('fs', 'nPer', 500, 10000)
+    20
+
+    Parameters
+    ----------
+    src_unit: string
+    dest_unit: string
+        Destination unit
+    value: numerical (e.g. integer or float)
+        Value to be converted
+
+    Returns
+    -------
+    converted unit : numerical value
+    '''
+    
+    def fs_to_nPer(req_fs, dsp_fs):
+        if dsp_fs < req_fs:
+            raise SamplingRateError(dsp_fs, req_fs)
+        return int(dsp_fs/req_fs)
+    
+    def nPer_to_fs(nPer, dsp_fs):
+        return dsp_fs/nPer
+    
+    def n_to_s(n, dsp_fs):
+        return n/dsp_fs
+    
+    def s_to_n(s, dsp_fs):
+        return int(s*dsp_fs)
+
+    def ms_to_n(ms, dsp_fs):
+        return int(ms*1e-3*dsp_fs)
+
+    def n_to_ms(n, dsp_fs):
+        return n/dsp_fs*1e3
+   
+    def s_to_nPow2(s, dsp_fs):
+        return nextpow2(s_to_n(s, dsp_fs))
+
+    fun = '%s_to_%s' % (src_unit, dest_unit)
+    return locals()[fun](value, dsp_fs)
+
---
+++ dsp_circuit.py
@@ -0,0 +1,208 @@
+import atexit
+import numpy as np
+
+from util import get_cof_path, connect_zbus, connect
+from dsp_buffer import ReadableDSPBuffer, WriteableDSPBuffer
+from convert import convert
+from dsp_error import DSPError
+from constants import RCX_COEFFICIENT
+
+import logging
+log = logging.getLogger(__name__)
+
+class DSPCircuit(object):
+
+    def __init__(self, circuit_name, device_name, load=True):
+        self.device_name = device_name
+        self.circuit_name = circuit_name
+        # Hint for Matlab users: _iface is the same COM object a Matlab user
+        # typically works with when they call actxserver('RPco.X').  It supports
+        # the exact same methods as the Matlab version.
+        self._iface = connect(device_name)
+        self._zbus  = connect_zbus()
+        self._cof_path = get_cof_path(circuit_name)
+        if load:
+            self.load()
+            import atexit
+            atexit.register(self.stop)
+        else:
+            self.read()
+        self.inspect()
+
+    def __getstate__(self):
+        '''
+        Provides support for pickling, which is required by the multiprocessing
+        module for launching a new process.  _iface and _zbus are PyIDispatch
+        objects which do not support pickling, so we just delete them and pickle
+        the rest.
+        '''
+        state = self.__dict__.copy()
+        del state['_iface']
+        del state['_zbus']
+        return state
+
+    def __setstate__(self, state):
+        '''
+        Loads the state and reconnects the COM objects
+        '''
+        self.__dict__.update(state)
+        self._iface = connect(self.device_name)
+        self._zbus  = connect_zbus()
+        self.read()
+
+    def read(self):
+        if not self._iface.ReadCOF(self._cof_path):
+            mesg = "Unable to read %s" % self._cof_path
+            raise DSPError(self, mesg)
+
+    def load(self):
+        '''
+        Clear DSP RAM set all variables to default value
+
+        The circuit is reloaded from disk, so any recent edits to the circuit
+        will be reflected in the running program.
+        '''
+        # Note that ClearCOF alone does not apear to reset all variables to
+        # default value, so we reload the circuit as well.
+        if not self._iface.ClearCOF():
+            mesg = "Unable to clear %s buffers" % self
+            raise DSPError(self, mesg)
+        if not self._iface.LoadCOF(self._cof_path):
+            mesg = 'Unable to load %s' % self._cof_path
+            raise DSPError(self, mesg)
+        log.info("Reloaded %s", self)
+        self.trigger('A', 'low')
+        self.trigger('B', 'low')
+
+    def inspect(self):
+        '''
+        Determine what tags are available in the microcode
+        '''
+        # Inspect COF for available tags and their type/size
+        num_tags = self._iface.GetNumOf('ParTag')
+        self.tags = {}
+        for i in range(num_tags):
+            name = self._iface.GetNameOf('ParTag', i+1)
+            if not name.startswith('%'):
+                tag_size = self._iface.GetTagSize(name)
+                if tag_size == 0:
+                    mesg = "Attempted to initialize invalid tag %s" % name
+                    raise DSPError(self, mesg)
+                tag_type = self._iface.GetTagType(name)
+                self.tags[name] = (tag_size, tag_type)
+                log.info("%s: found %s (size %d, type %s)", self, name,
+                          tag_size, tag_type)
+        self.fs = self._iface.GetSFreq()
+
+    def cget_tag(self, name, tag_unit, val_unit):
+        '''
+        Enhanced version of `get_tag` that returns value in requested unit
+
+        Parameters
+        ----------
+        name : str
+            Tag name
+        tag_unit : str
+            Unit of tag
+        val_unit : str
+            Requested unit
+        '''
+        value = self.get_tag(name)
+        return convert(tag_unit, val_unit, value, self.fs)
+
+    def cset_tag(self, name, value, val_unit, tag_unit):
+        '''
+        Enhanced version of `set_tag` that converts the value before setting
+
+        Parameters
+        ----------
+        name : str
+            Tag name
+        val_unit : str
+            Requested unit
+        tag_unit : str
+            Unit of tag
+        '''
+        value = convert(val_unit, tag_unit, value, self.fs)
+        self.set_tag(name, value)
+
+    def get_tag(self, name):
+        '''
+        Analogue of RPco.X.GetTagVal
+        '''
+        # Check to see if tag exists
+        if name not in self.tags:
+            raise DSPError(self, "Tag %s does not exist" % name)
+        if self.tags[name][0] != 1:
+            raise DSPError(self, "Tag %s is not a scalar value" % name)
+        value = self._iface.GetTagVal(name)
+        log.debug("Get %s:%s is %r", self, name, value)
+        return value
+
+    def set_tag(self, name, value):
+        '''
+        Analogue of RPco.X.SetTagVal
+        '''
+        # Check to see if tag exists
+        if name not in self.tags:
+            raise DSPError(self, "Tag %s does not exist" % name)
+        if self.tags[name][0] != 1:
+            raise DSPError(self, "Tag %s is not a scalar value" % name)
+        if not self._iface.SetTagVal(name, value):
+            mesg = "Unable to set tag %s to %r" % (name, value)
+            raise DSPError(self, mesg)
+        log.info("Set %s:%s to %r", self, name, value)
+
+    def set_coefficients(self, name, value):
+        tag_size, tag_type = self.tags[name]
+        if tag_type != RCX_COEFFICIENT:
+            raise DSPError(self, "Tag %s is not a coefficient buffer" % name)
+        if tag_size != len(value):
+            mesg = "Exactly %d values must be written to tag %s"
+            raise DSPError(self, mesg % (tag_size, name))
+        # Per conversation with TDT's tech support (Mark Hanus and Chris
+        # Walters), RPvds' CoefLoad appears to be broken.  It's not clear
+        # whether there are plans to fix this in future releases.  They
+        # indicated we should connect a tag directly to the coefficient buffer
+        # and use WriteTagV.  Coefficients must be a 1D array (even for matrix
+        # multiplication operations.
+        self._iface.WriteTagV(name, 0, value)
+
+    def start(self):
+        '''
+        Analogue of RPco.X.Run
+        '''
+        log.debug("starting %s", self)
+        self._iface.Run()
+
+    def stop(self):
+        '''
+        Analogue of RPco.X.Halt
+        '''
+        log.debug("stopping %s", self)
+        self._iface.Halt()
+
+    def trigger(self, trigger, mode='pulse'):
+        # Convert mode string to the corresponding integer
+        mode_enum = dict(pulse=0, high=1, low=2)
+        if trigger == 'A':
+            self._zbus.zBusTrigA(0, mode_enum[mode], 10)
+        elif trigger == 'B':
+            self._zbus.zBusTrigB(0, mode_enum[mode], 10)
+        elif (1 <= trigger < 10) and mode == 'pulse':
+            self._iface.SoftTrg(trigger)
+        else:
+            raise ValueError, "Unsupported trigger mode %s %s" % (trigger, mode)
+        log.info('Trigger %r %s', trigger, mode)
+
+    def get_buffer(self, data_tag, mode, *args, **kw):
+        if mode == 'w':
+            return WriteableDSPBuffer(self, data_tag, *args, **kw)
+        elif mode == 'r':
+            return ReadableDSPBuffer(self, data_tag, *args, **kw)
+
+    def clear_buffer(self, name):
+        self._iface.ZeroTag(name)
+
+    def __str__(self):
+        return "{0}:{1}".format(self.device_name, self.circuit_name)
