914c8f8559d3aaec1ce5cacd53afc1d6f858750c
Brad Buran <bburan@alum.mit.edu>
2011-04-17 14:52:19 -0400

    More informative str representation of DSPCircuit plus better error handling

--- dsp_error.py
+++ dsp_error.py
@@ -1,12 +1,13 @@
 class DSPError(BaseException):
 
     def __init__(self, device, mesg):
-        self.error = device._zbus.GetError()
-        if self.error == '':
-            self.error = "No error code returned"
+        #self.error = error
+        #if self.error == '':
+            #self.error = "No error code returned"
         self.device = device
         self.mesg = mesg
 
     def __str__(self):
-        return '%s: %s (zBUS error code: %s)' % (self.device, self.mesg,
-                self.error)
+        #return '%s: %s (zBUS error code: %s)' % (self.device, self.mesg,
+        #        self.error)
+        return '{}: {}'.format(self.device, self.mesg)
