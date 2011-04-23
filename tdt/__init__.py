f1e51a7bf04bbeb22866abffacf611943767b6ed
Brad Buran <bburan@alum.mit.edu>
2011-04-23 15:19:16 -0400

    Moved DSPProject to its own file

--- tdt/__init__.py
+++ tdt/__init__.py
@@ -1,5 +1,8 @@
 try:
-    from dsp_process import DSPProcess, DSPProject
+    from dsp_project import DSPProject
+    from dsp_process import DSPProcess
     from dsp_circuit import DSPCircuit
+    from dsp_buffer import DSPBuffer
+    from dsp_error import DSPError
 except:
     pass
