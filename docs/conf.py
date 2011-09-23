ffe1252ceac7b64e37cb7919071c22f424d6c9ca
Brad Buran <bburan@alum.mit.edu>
2011-09-23 18:39:01 -0400

    Mocking modules to prevent sphinx.ext.autodoc from failing on import

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,16 @@
 
 import sys, os
 
+# Mock out the modules so that sphinx autodoc can run without requiring these
+# platform dependencies
+class Mock(object):
+    def __init__(self, *args):
+        pass
+    def __getattr__(self, name):
+        return Mock
+for mod_name in ('numpy', 'win32com'):
+    sys.modules[mod_name] = Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
