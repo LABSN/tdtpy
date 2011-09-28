1868a21ce86d90481de3400938931fc644be58b9
Brad Buran <bburan@alum.mit.edu>
2011-09-28 11:16:55 -0400

    Changed default block size to 1 for tdt.DSPBuffer
    * Many people likely do not pay attention to the block size when writing code
      that uses TDTPy, so they may be surprised when their read() does not work if
      there isn't enough data in the buffer to produce a full block of data.
    * Those who know about block size likely will want to optimize the block size
      for their specific use case, so it doesn't really make sense to try and set
      a default value that is > 1.

---
+++ tdt/abstract_ring_buffer.py
@@ -0,0 +1,121 @@
+import numpy as np
+import logging
+log = logging.getLogger(__name__)
+
+def wrap(length, offset, buffer_size):
+    if (offset+length) > buffer_size:
+        a_length = buffer_size-offset
+        b_length = length-a_length
+        return ((offset, a_length), (0, b_length))
+    else:
+        return ((offset, length), )
+
+def pending(old_idx, new_idx, buffer_size):
+    '''
+    Returns the number of slots that have been filled with data since the
+    last read.
+    '''
+    if new_idx < old_idx:
+        return buffer_size-old_idx+new_idx
+    else:
+        return new_idx-old_idx
+
+class AbstractRingBuffer(object):
+    '''
+    Subclasses must provide the following attributes (either by setting them in
+    the __init__ method or providing property getter/setters):
+
+        * read_index
+        * write_index
+        * size
+        * channels
+        * block_size
+
+    Also, provide implementation of the following methods:
+        * _read(self, offset, length)
+        * _write(self, offset, data)
+
+    The following attributes provide information on the state of the buffer:
+        * total_samples_written
+        * total_samples_read
+
+    TODO:
+        * write_cycle
+        * read_cycle
+
+    '''
+    total_samples_written   = 0
+    total_samples_read      = 0
+
+    # Note that read_index and write_index may be overriden as property
+    # getter/setters in subclasses.
+    read_index              = 0
+    write_index             = 0
+
+    def pending(self):
+        '''
+        Number of filled slots waiting to be read
+        '''
+        return pending(self.read_index, self.write_index, self.size)
+
+    def blocks_pending(self):
+        '''
+        Number of filled slots waiting to be read
+        '''
+        return int(self.pending()/self.block_size)*self.block_size
+
+    def available(self):
+        '''
+        Number of empty slots available for writing
+        '''
+        return self.size-self.pending()
+
+    def blocks_available(self):
+        return int(self.available()/self.block_size)*self.block_size
+
+    def read_all(self):
+        return self.read(self.pending())
+
+    def read(self, samples=None):
+        if samples is None:
+            samples = self.blocks_pending()
+        elif samples > self.pending():
+            mesg = 'Attempt to read %r samples failed because only ' + \
+                   '%r slots are available for read'
+            raise ValueError, mesg % (samples, self.pending())
+
+        data = self._get_empty_array(samples)
+        samples_read = 0
+        for o, l in wrap(samples, self.read_index, self.size):
+            data[..., samples_read:samples_read+l] = self._read(o, l)
+            samples_read += l
+        self.read_index = (o+l) % self.size 
+        self.total_samples_read += samples_read
+        return data
+
+    def write(self, data, force=False):
+        available = self.available()
+        samples = data.shape[-1]
+        if samples == 0:
+            return
+        elif not force and (samples > available):
+            mesg = 'Attempt to write %d samples failed because only ' + \
+                   '%d slots are available for write'
+            raise ValueError, mesg % (samples, available)
+
+        samples_written = 0
+        for o, l in wrap(samples, self.write_index, self.size):
+            if not self._write(o, data[..., samples_written:samples_written+l]):
+                raise SystemError, 'Problem with writing data to buffer'
+            samples_written += l
+        self.write_index = (o+l) % self.size 
+        self.total_samples_written += samples_written
+        return samples_written
+
+    def reset_read(self, index=None):
+        '''
+        Reset the read index
+        '''
+        if index is None:
+            index = self._iface.GetTagVal(self.idx_tag)
+        self.read_index = index
---
+++ tdt/shared_ring_buffer.py
@@ -0,0 +1,109 @@
+import time
+import logging
+log = logging.getLogger(__name__)
+import numpy as np
+
+from abstract_ring_buffer import AbstractRingBuffer
+
+class SharedRingBuffer(AbstractRingBuffer):
+    '''
+    Implementation that reads/writes data to a shared memory location.  Used for
+    inter-process communication.  The assumption is one process only reads and
+    the other process only writes.  Does not support single-write, multi-read
+    (i.e. only one process can do the reading).
+
+    Cache *must* be a view into a Numpy array.  use shmem_to_ndarray and reshape
+    '''
+
+    def __init__(self, cache, iwrite, iread, ioffset, condition, circuit):
+        self._ioffset = ioffset
+        self._iwrite = iwrite
+        self._iread = iread
+        self._cache = cache
+        self._condition = condition
+        self._circuit = circuit
+        self._dtype = self._cache.dtype
+        self.channels, self.size = self._cache.shape
+        self.block_size = 1
+
+        self._ioffset.value = -1
+        self._iwrite.value = 0
+        self._iread.value = 0
+        self._processed = False
+    def _get_empty_array(self, samples):
+        return np.empty((self.channels, samples), dtype=self._dtype)
+
+    def _get_read_index(self):
+        return self._iread.value
+
+    def _set_read_index(self, value):
+        self._iread.value = int(value)
+
+    read_index = property(_get_read_index, _set_read_index)
+
+    def _get_write_index(self):
+        return self._iwrite.value
+
+    def _set_write_index(self, value):
+        self._iwrite.value = int(value)
+
+    write_index = property(_get_write_index, _set_write_index)
+
+    def _read(self, offset, length):
+        return self._cache[..., offset:offset+length]
+
+    def _write(self, offset, data):
+        samples = data.shape[-1]
+        self._cache[..., offset:offset+samples] = data
+        return samples
+
+    def should_set(self):
+        return self._ioffset.value >= 0
+
+    # Read and write should be locked to prevent concurrent access by the two
+    # proceses.  Even though one process is read-only and the other is
+    # write-only, I haven't had a chance to rigorously debug what happens if we
+    # don't lock access to the read/write.  The lock should be a re-entrant
+    # lock.  Since set() acquires a lock and then calls write(), this allows
+    # write() to acquire the same lock as well.  A simple, non-reentrant lock
+    # would prevent write() from acquiring lock().
+    def read(self, samples=None):
+        with self._condition:
+            return super(SharedRingBuffer, self).read(samples)
+
+    def write(self, data, timeout=None):
+        with self._condition:
+            result = super(SharedRingBuffer, self).write(data)
+            # Now, wait till the subprocess acknowledges that it has recieved
+            # the data and has uploaded it to the hardware before returning.  By
+            # telling _condition to wait, it will release the lock, and then
+            # wait for the other process to issue a _condition.notify() signal.
+            # At this point, the lock returns to this thread.
+            self._condition.wait(timeout)
+        return result
+
+    def set(self, data, timeout=None):
+        with self._condition:
+            # Locking is very important here because we need to ensure that both
+            # _ioffset and the data are written before the other process has
+            # access to it.
+            self._ioffset.value = 0
+            self.write(data, timeout=timeout)
+
+    def clear(self):
+        with self._condition:
+            self._circuit.clear_buffer(self.data_tag)
+
+    def notify(self):
+        with self._condition:
+            self._condition.notify()
+
+class ReadableSharedRingBuffer(SharedRingBuffer):
+
+    def _write(self, offset, data):
+        raise NotImplementedError
+
+class WriteableSharedRingBuffer(SharedRingBuffer):
+
+    def _read(self, offset, data):
+        raise NotImplementedError
