3e3eaa8cee1a141714e5bc19053cb86728bfbdaa
Brad Buran <info@bradburan.com>
2023-01-09 19:03:08 -0500

    Remove obsolete logging messages

--- tdt/dsp_buffer.py
+++ tdt/dsp_buffer.py
@@ -413,9 +413,7 @@ class ReadableDSPBuffer(DSPBuffer):
     def _get_write_cycle(self):
         if self.cycle_tag is None:
             return None
-        cycle = int(self.circuit.get_tag(self.cycle_tag))
-        log.debug("%s: cycle", cycle)
-        return cycle
+        return int(self.circuit.get_tag(self.cycle_tag))
 
     write_cycle = property(_get_write_cycle)
 
@@ -446,9 +444,7 @@ class WriteableDSPBuffer(DSPBuffer):
     def _get_read_cycle(self):
         if self.cycle_tag is None:
             return None
-        cycle = self.circuit.get_tag(self.cycle_tag)
-        log.debug("%s: cycle", cycle)
-        return cycle
+        return self.circuit.get_tag(self.cycle_tag)
 
     read_cycle = property(_get_read_cycle)
 
