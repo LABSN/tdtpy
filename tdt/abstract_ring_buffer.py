Fix race condition when checking for buffer under and overflow (#23)

tdt/abstract_ring_buffer.py
@@ -1,6 +1,8 @@
 import logging
 log = logging.getLogger(__name__)
 
+import numpy as np
+
 
 def wrap(length, offset, buffer_size):
     if (offset+length) > buffer_size:
@@ -22,44 +24,31 @@ def span(old_cycle, old_idx, new_cycle, new_idx, buffer_size):
                  start, old_cycle, old_idx, end, new_cycle, new_idx, buffer_size)
         raise ValueError('Start sample higher than end sample')
     if (end - start) > buffer_size:
+        log.info('Start sample %d (C %d, I %d), end sample %d (C %d, I %d), buffer %d',
+                 start, old_cycle, old_idx, end, new_cycle, new_idx, buffer_size)
         raise ValueError('Number of slots exceeds buffer size')
     return end-start
 
 
-class AbstractRingBuffer(object):
+class AbstractRingBuffer:
     '''
     Subclasses must provide the following attributes (either by setting them in
     the __init__ method or providing property getter/setters):
 
         * read_index
         * write_index
+        * read_cycle
+        * write_cycle
         * size
         * channels
         * block_size
+        * total_samples_written
+        * total_samples_read
 
     Also, provide implementation of the following methods:
         * _read(self, offset, length)
         * _write(self, offset, data)
-
-    The following attributes provide information on the state of the buffer:
-        * total_samples_written
-        * total_samples_read
     '''
-    # This should be write_cycle * size + write_index
-    total_samples_written = 0
-
-    # This should be read_cycle * size + read_index
-    total_samples_read = 0
-
-    # This tracks the current index in the buffer. The read_index and
-    # write_index may be overriden as property getter/setters in subclasses.
-    read_index = 0
-    write_index = 0
-
-    # This tracks the current "loop" of the buffer. The read_cycle and
-    # write_cycle may be overridden as property getter/setters in subclasses.
-    read_cycle = 0
-    write_cycle = 0
 
     def _offset_to_index(self, offset):
         if offset is None:
@@ -76,9 +65,11 @@ class AbstractRingBuffer(object):
     def pending(self):
         '''
         Number of filled slots waiting to be read
+
         '''
+        self.latch()
         return span(self.read_cycle, self.read_index, self.write_cycle,
-                       self.write_index, self.size)
+                    self.write_index, self.size)
 
     def blocks_pending(self):
         '''
@@ -96,6 +87,7 @@ class AbstractRingBuffer(object):
             If specified, return number of samples relative to offset. Offset
             is relative to beginning of acquisition.
         '''
+        self.latch()
         write_cycle, write_index = self._offset_to_index(offset)
         if (self.total_samples_written == 0) and (self.read_index == 0):
             return self.size
@@ -114,6 +106,13 @@ class AbstractRingBuffer(object):
         return self.read(self.pending())
 
     def read(self, samples=None):
+        '''
+        Parameters
+        ----------
+        samples : int
+            Number of samples to read. If None, read all samples acquired since
+            last call to read.
+        '''
         try:
             if samples is None:
                 samples = self.blocks_pending()
@@ -124,16 +123,10 @@ class AbstractRingBuffer(object):
         except ValueError:
             raise IOError('Read was too slow and unread samples were overwritten')
 
-        data = self._get_empty_array(samples)
-        samples_read = 0
-        for i, (o, l) in enumerate(wrap(samples, self.read_index, self.size)):
-            data[..., samples_read:samples_read+l] = self._read(o, l)
-            samples_read += l
-            if i > 0:
-                self.read_cycle += 1
-
-        self.read_index = (o+l) % self.size
-        self.total_samples_read += samples_read
+        data = [self._read(o, l) for o, l in wrap(samples, self.read_index, self.size)]
+        data = np.concatenate(data, axis=-1)
+        self.total_samples_read += samples
+        self.read_cycle, self.read_index = divmod(self.total_samples_read, self.size)
         return data
 
     def write(self, data, offset=None):
@@ -143,10 +136,8 @@ class AbstractRingBuffer(object):
         except ValueError:
             raise IOError('Write was too slow and old samples were regenerated')
         samples = data.shape[-1]
-        log.debug('Current write cycle %d and index %d', self.write_cycle,
-                  self.write_index)
-        log.debug('%d samples available for write starting at %d', available,
-                  samples)
+        log.debug('Current write cycle %d and index %d with %d samples available to write',
+                  self.write_cycle, self.write_index, available)
 
         if samples == 0:
             return

tests/test_tdtpy.py
@@ -16,7 +16,7 @@ def project():
 
 @pytest.fixture
 def circuit(project):
-    circuit = project.load_circuit('RZ6-debugging.rcx', 'RZ6', 1)
+    circuit = project.load_circuit('RZ6-debugging.rcx', 'RZ6', 1, latch_trigger=1)
     circuit.start()
     yield circuit
     circuit.stop()
@@ -28,14 +28,18 @@ def ao1(circuit):
 
 
 @pytest.fixture
+def ao2(circuit):
+    return circuit.get_buffer('ao2', 'w')
+
+
+@pytest.fixture
 def ai1(circuit):
     return circuit.get_buffer('ai1', 'r')
 
 
 @pytest.fixture
-def ai3(circuit):
-    # This tag can be downsampled
-    return circuit.get_buffer('ai3', 'r', dec_factor=4)
+def ai2(circuit):
+    return circuit.get_buffer('ai2', 'r')
 
 
 def test_circuit_load(circuit):
@@ -43,10 +47,9 @@ def test_circuit_load(circuit):
     circuit.stop()
 
 
-def test_buffer_fs(ao1, ai1, ai3):
+def test_buffer_fs(ao1, ai1):
     assert ao1.fs == BASE_FS
     assert ai1.fs == BASE_FS
-    assert ai3.fs == (BASE_FS / 4)
 
 
 def test_circuit_write_read(project, ao1, ai1):
@@ -57,57 +60,36 @@ def test_circuit_write_read(project, ao1, ai1):
     time.sleep(1)
 
     # There is a two-sample delay in the circuit
-    read_samples = ai1.read()[0, 2:n+2]
+    read_samples = ai1.read()[0, :n]
     np.testing.assert_allclose(write_samples, read_samples)
 
 
 def test_buffer_detect_dec_factor(circuit):
     # The default decimation factor is 8 by default
-    ai3 = circuit.get_buffer('ai3', 'r')
-    assert ai3.fs == (BASE_FS / 8)
+    ai_dec1 = circuit.get_buffer('ai_dec1', 'r')
+    assert ai_dec1.fs == (BASE_FS / 8)
 
 
-def test_circuit_write_read_dec(project, ao1, ai3):
+@pytest.mark.parametrize("dec_factor", [1, 2, 4, 8])
+def test_circuit_write_read_dec(project, circuit, ao1, dec_factor):
+    ai_dec = circuit.get_buffer('ai_dec1', 'r', dec_factor=dec_factor)
+    assert ai_dec.fs == (BASE_FS / dec_factor)
+
     n = round(ao1.fs / 10)
     t = np.arange(n) / ao1.fs
     write_samples = np.sin(2 * np.pi * 50 * t)
     ao1.write(write_samples)
 
     project.trigger('A', 'high')
-    time.sleep(0.1)
-
-    n_dec = int(n / 4)
-
-    # I'm not sure why we need to discard the first sample here.
-    read_samples_dec = ai3.read()[0, 1:n_dec+1]
-
-    # Due to the two-sample delay, we need to decimate the write
-    # buffer starting at sample two.
-    write_samples_dec = write_samples[2::4]
-    np.testing.assert_allclose(write_samples_dec, read_samples_dec)
-
-
-def test_circuit_incremential_write_read(project, ao1, ai1):
-    n = round(ao1.fs)
-    write = []
-    read = []
-
+    read_samples_dec = []
     for i in range(10):
-        write.append(np.random.uniform(size=n))
+        time.sleep(0.05)
+        read_samples_dec.append(ai_dec.read())
 
-    ao1.write(write[0])
-    project.trigger('A', 'high')
-    for s in write[1:]:
-        time.sleep(0.5)
-        ao1.write(s)
-        read.append(ai1.read())
-    time.sleep(11*0.5)
-    read.append(ai1.read())
-
-    write = np.concatenate(write, axis=-1)
-    read = np.concatenate(read, axis=-1)[0]
-    # Be sure to correct for 2 sample delay
-    np.testing.assert_allclose(write, read[2:len(write)+2])
+    n_dec = int(n / dec_factor)
+    read_samples_dec = np.concatenate(read_samples_dec, axis=-1)[0, :n_dec]
+    write_samples_dec = write_samples[::dec_factor][:n_dec]
+    np.testing.assert_allclose(write_samples_dec, read_samples_dec)
 
 
 def test_circuit_write_too_slow(project, ao1):
@@ -140,3 +122,86 @@ def test_circuit_read_too_slow(project, ai1):
     time.sleep(1)
     with pytest.raises(IOError, match='Read was too slow and unread samples were overwritten'):
         ai1.read()
+
+
+def test_circuit_read_wrap(project, ai1, ai2):
+    project.trigger('A', 'high')
+    d1 = []
+    d2 = []
+    for i in range(15):
+        time.sleep(0.1)
+        d1.append(ai1.read())
+        d2.append(ai2.read())
+    project.trigger('A', 'low')
+    d1.append(ai1.read())
+    d2.append(ai2.read())
+    d1 = np.concatenate(d1, axis=1)
+    d2 = np.concatenate(d2, axis=1)
+    assert d1.shape[-1] == ((ai1.read_cycle * ai1.n_slots) + ai1.read_index)
+    assert d2.shape[-1] == ((ai2.read_cycle * ai2.n_slots) + ai2.read_index)
+    assert d1.shape == d2.shape
+    assert ai1.read_cycle != 0
+
+
+@pytest.mark.parametrize("dec_factor", [1, 2, 4, 8])
+def test_circuit_dec_read_wrap(project, circuit, dec_factor):
+    # Verify that decimated buffer reads properly wrap around
+    ai1 = circuit.get_buffer('ai_dec1', 'r', dec_factor=dec_factor)
+    read_time = ai1.n_samples / ai1.fs
+    print(read_time)
+    project.trigger('A', 'high')
+    d1 = []
+    for i in range(15):
+        time.sleep(read_time * 0.1)
+        d1.append(ai1.read())
+    project.trigger('A', 'low')
+    d1.append(ai1.read())
+    d1 = np.concatenate(d1, axis=1)
+    assert d1.shape[-1] == ((ai1.read_cycle * ai1.n_slots) + ai1.read_index)
+    assert ai1.read_cycle != 0
+
+
+def test_circuit_write_wrap(project, ao1, ao2, ai1, ai2):
+    d1 = np.random.uniform(size=ao1.available())
+    ao1.write(d1)
+    w1 = [d1]
+
+    d2 = np.random.uniform(size=ao2.available())
+    ao2.write(d2)
+    w2 = [d2]
+    r1 = []
+    r2 = []
+
+    project.trigger('A', 'high')
+    for i in range(15):
+        time.sleep(0.1)
+        d1 = np.random.uniform(size=ao1.available())
+        ao1.write(d1)
+        w1.append(d1)
+        d2 = np.random.uniform(size=ao2.available())
+        ao2.write(d2)
+        w2.append(d2)
+
+        r1.append(ai1.read())
+        r2.append(ai2.read())
+
+    project.trigger('A', 'low')
+    d1 = np.random.uniform(size=ao1.available())
+    ao1.write(d1)
+    w1.append(d1)
+    d2 = np.random.uniform(size=ao2.available())
+    ao2.write(d2)
+    w2.append(d2)
+    r1.append(ai1.read())
+    r2.append(ai2.read())
+
+    w1 = np.concatenate(w1, axis=-1)
+    w2 = np.concatenate(w2, axis=-1)
+    r1 = np.concatenate(r1, axis=-1)[0]
+    r2 = np.concatenate(r2, axis=-1)[0]
+
+    assert r1.shape == r2.shape
+    n = r1.shape[0]
+
+    np.testing.assert_array_almost_equal(w1[:n], r1)
+    np.testing.assert_array_almost_equal(w2[:n], r2)