819e46dca5e256a8042230c22fd54c3ef8a43ada
Brad Buran <info@bradburan.com>
2023-01-27 10:09:48 -0800

    Finishing tweaks on pyproject.toml to ensure version works

--- tdt/__init__.py
+++ tdt/__init__.py
@@ -1,4 +1,3 @@
-from importlib.metadata import version, PackageNotFoundError
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,7 +10,15 @@ from .dsp_buffer import DSPBuffer
 from .dsp_error import DSPError
 
 try:
+    from importlib.metadata import version
     __version__ = version("TDTPy")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = '0.0.0'
+except Exception:
+    # Catch a generic exception which will handle both PackageNotFoundError and
+    # ImportError (attempting to catch PackageNotFoundError is a bit tricky if
+    # importlib.metadata is not available).
+    try:
+        from .version import __version__
+    except ImportError:
+        # package is not installed. probably some munging of Python path going
+        # on here.
+        __version__ = '0.0.0'
